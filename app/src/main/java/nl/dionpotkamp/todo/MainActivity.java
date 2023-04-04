a18e05730b11c3900b78cde87b082de711254f95
Dion Potkamp <dion.potkamp@gmail.com>
2023-04-04 14:21:12 -0400

    improve swipe action delete/update

--- app/src/main/java/nl/dionpotkamp/todo/MainActivity.java
+++ app/src/main/java/nl/dionpotkamp/todo/MainActivity.java
@@ -1,9 +1,14 @@
 package nl.dionpotkamp.todo;
 
 import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -14,6 +19,7 @@ import nl.dionpotkamp.todo.adapters.TodoAdapter;
 import nl.dionpotkamp.todo.databinding.ActivityMainBinding;
 import nl.dionpotkamp.todo.models.Todo;
 import nl.dionpotkamp.todo.utils.DBControl;
+
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
@@ -35,29 +41,7 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
         swipeRefreshLayout = findViewById(R.id.swipeContainer);
         swipeRefreshLayout.setOnRefreshListener(this);
 
-        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
-            @Override
-            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
-                return false;
-            }
-
-            @Override
-            public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
-                int position = viewHolder.getAdapterPosition();
-                TodoAdapter adapter = (TodoAdapter) recyclerview.getAdapter();
-
-                Todo todo = adapter.getTodoAt(position);
-                if (direction == ItemTouchHelper.RIGHT) {
-                    todo.delete();
-                    adapter.deleteItem(position);
-                } else if (direction == ItemTouchHelper.LEFT) {
-                    Intent intent = new Intent(MainActivity.this, ToDoCreateUpdate.class);
-                    intent.putExtra("isUpdate", true);
-                    intent.putExtra("id", todo.getId());
-                    startActivity(intent);
-                }
-            }
-        });
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(new UpdateDeleteSwipe(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT));
         itemTouchHelper.attachToRecyclerView(recyclerview);
 
         FloatingActionButton fab = binding.fab;
@@ -96,5 +80,74 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
     public void onRefresh() {
         refreshList();
         swipeRefreshLayout.setRefreshing(false);
+    }
+
+    // UpdateDeleteSwipe.onChildDraw adapted from https://stackoverflow.com/a/33344173/10463118
+    class UpdateDeleteSwipe extends ItemTouchHelper.SimpleCallback {
+        public UpdateDeleteSwipe(int dragDirs, int swipeDirs) {
+            super(dragDirs, swipeDirs);
+        }
+
+        @Override
+        public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+            return false;
+        }
+
+        @Override
+        public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+            int position = viewHolder.getAdapterPosition();
+            TodoAdapter adapter = (TodoAdapter) recyclerview.getAdapter();
+
+            if (adapter == null)
+                return;
+
+            Todo todo = adapter.getTodoAt(position);
+            if (direction == ItemTouchHelper.RIGHT) {
+                todo.delete();
+                adapter.deleteItem(position);
+                Toast.makeText(MainActivity.this, "Todo Deleted", Toast.LENGTH_SHORT).show();
+            } else if (direction == ItemTouchHelper.LEFT) {
+                Intent intent = new Intent(MainActivity.this, ToDoCreateUpdate.class);
+                intent.putExtra("isUpdate", true);
+                intent.putExtra("id", todo.getId());
+                startActivity(intent);
+            }
+        }
+
+        // adapted from https://stackoverflow.com/a/33344173/10463118
+        public void onChildDraw(@NonNull Canvas c, @NonNull RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+            if (actionState == ItemTouchHelper.ACTION_STATE_SWIPE) {
+                View itemView = viewHolder.itemView;
+
+                Paint p = new Paint();
+
+                if (dX > 0) { // Swiping to the right (delete)
+                    // Set background color to red
+                    p.setARGB(155, 255, 0, 0);
+                    // Draw background
+                    c.drawRect((float) itemView.getLeft(), (float) itemView.getTop(), dX,
+                            (float) itemView.getBottom(), p);
+                    // Draw delete icon
+                    c.drawBitmap(BitmapFactory.decodeResource(getResources(), android.R.drawable.ic_menu_delete), (float) itemView.getLeft() + 20, (float) itemView.getTop() + ((float) itemView.getBottom() - (float) itemView.getTop() - 100) / 2, null);
+                } else if (dX < 0) { // Swiping to the left (update)
+                    // Set background color to orange
+                    p.setARGB(155, 255, 165, 0);
+                    // Draw background
+                    c.drawRect((float) itemView.getRight() + dX, (float) itemView.getTop(),
+                            (float) itemView.getRight(), (float) itemView.getBottom(), p);
+                    // Draw edit icon
+                    c.drawBitmap(BitmapFactory.decodeResource(getResources(), android.R.drawable.ic_menu_edit), (float) itemView.getRight() - 150, (float) itemView.getTop() + ((float) itemView.getBottom() - (float) itemView.getTop() - 100) / 2, null);
+                }
+
+                // Fade out the view as it is swiped out of the parent's bounds
+                final float alpha = 1.0f - Math.abs(dX) / (float) viewHolder.itemView.getWidth();
+                viewHolder.itemView.setAlpha(alpha);
+                viewHolder.itemView.setTranslationX(dX);
+
+            } else {
+                super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+            }
+        }
+
     }
 }
